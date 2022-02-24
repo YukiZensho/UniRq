c182d164fe573ad80409ba24b014b6f4ffc60de0
YukiZensho <yukizenshou@gmail.com>
2022-02-24 09:58:53 +0200

    c libs initialized

--- lab1/readadd.cpp
+++ lab1/readadd.cpp
@@ -1,12 +1,16 @@
-#include <iostream>
-#include <fstream>
 #include <stdlib.h>
-using namespace std;
+#include <stdio.h>
 
-ifstream fin("dummy");
-int a,s;
+FILE * f;
+int a=0,s=0;
+char c;
 int main(){
-	while(fin>>a)
-		s+=a;
+	f=fopen("dummy","r");
+	while(c = getc(f) != EOF){
+		c = fgetc(f);
+		if(c!=' '&&c!='\n')
+			{a=a*10+c-'0';}
+		else{
+			s+=a;a=0;}}
 	printf("%i is the result",s);
 }
