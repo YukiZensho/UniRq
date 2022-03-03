24d3b636c653c73f452afb6e2603dee976044c0f
YukiZensho <yukizenshou@gmail.com>
2022-03-03 09:28:03 +0200

    Lab2.1

--- lab2/NumberList.h
+++ lab2/NumberList.h
@@ -1,14 +1,6 @@
 #ifndef NUMBERLIST.H
 #define NUMBERLIST.H
 
-class NumberList{
-	int numbers[10];
-	int count;
-public:
-	void init();
-	bool Add( int x );
+class NumberList;
 
-	void Sort();
-	void Print();
-}
 #endif
--- lab2/main.cpp
+++ lab2/main.cpp
@@ -2,6 +2,11 @@
 
 
 int main(){
+	NumberList n;
+	n.init();
+	if(n.Add(5))
+		n.Sort();
+	n.Print();
 
-
+	return 0;
 }
