b53b9d8c2ff3283bc4a4a8e715c7db6f48363588
YukiZensho <yukizenshou@gmail.com>
2022-03-10 09:46:03 +0200

    Lab3.2

--- lab3/canvas.hpp
+++ lab3/canvas.hpp
@@ -1,15 +1,21 @@
 #ifndef	CANVAS
 #define	CANVAS
+#include <math.h>
 
-#include <iostream>
 class canvas{
-	int x[100],y[100],height,width;
+	int x[100][100],height,width;
 	public:
 		canvas(int width,int height){
 			this->width=width;
 			this->height=height;
-			std::cout<<"aa";
 		}
+	void DrawCircle(int x, int y, int ray, char ch){
+		for(int i=0;i<width;i++)
+			for(int j=0;j<height;j++)
+				if(sqrt((this->x[i][j]-x)*(this->x[i][j]-x)+(this->x[i][j]-y)*(this->x[i][j]-y))==ray)
+					this->x[i][j]=ch;
+	}
+	
 };
 
 #endif
