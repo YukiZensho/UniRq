98883274400d810d886f69642c11e7299f9c619b
YukiZensho <yukizenshou@gmail.com>
2022-03-17 09:39:57 +0200

    Lab4.4

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -6,6 +6,6 @@ int main(){
     cout<<"TEST\n";
     //sort s(5,10,100);
 	int a[100]={15,4,2,4};
-	sort s(4,a);
+	sort s("10,14,24");
     s.print();
 }
--- lab4/sort.hpp
+++ lab4/sort.hpp
@@ -2,9 +2,25 @@
 #define	SORT
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
 class sort{
 	int e[100],q;
 	public:
+
+	void InsertSort(bool ascendent=false);
+
+    void QuickSort(bool ascendent=false);
+
+    void BubbleSort(bool ascendent=false);
+
+	int  GetElementsCount(){
+		return q;	
+	}
+
+    int  GetElementFromIndex(int index){
+		return e[q];
+	}
+
 	sort(int q, int l, int h){
 		this->q = q;
 		while(q--)	this->e[q]=rand()%(h-l)+l;
@@ -13,6 +29,16 @@ class sort{
 		this->q = q;
 		while(this->q--)	this->e[this->q]=e[this->q];
 		this->q=q;
+	}
+	sort(std::string s){
+		this->q=0;
+		int j=0;
+		for(int i=0;i<s.size();i++)
+			if(s[i]==','){
+				for(;j<i;j++)
+					this->e[this->q]=s[j]-'0';
+				this->q+=1;}
+				
 	}
 	void print(){
 		for(int i=0;i<this->q;i++)
